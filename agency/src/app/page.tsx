25b3a9f9927488746d20a9e51d6d4d0ba804e428
fuyofulo <pheonixdiaz625@gmail.com>
2025-04-19 18:11:51 +0530

    fixed build error

--- agency/src/app/page.tsx
+++ agency/src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/hero/Hero";
 import { Services } from "@/components/services/Services";
 import { ProcessFlow } from "@/components/services/ProcessFlow";
-import { Testimonials } from "@/components/testimonials/Testimonials";
 import { Contact } from "@/components/contact/Contact";
 import { Navbar } from "@/components/navbar/Navbar";
 import { VapiUI } from "@/functions/Vapi";
